cbbf2d0d90ba3b8b26b6167626ff3ffe3ff4399b
erwinaris <vendettaajah@gmail.com>
2020-11-08 10:33:21 +0700

    rev1

---
+++ plugins/js/initialize.js
@@ -0,0 +1,8 @@
+const dropDown = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(dropDown);
+
+const collapse = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapse);
+
+const box = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(box);
