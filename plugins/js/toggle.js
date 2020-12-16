770a1cd96cd8c18e440296cd8a5d71f9bce5ef6a
erwinaris <vendettaajah@gmail.com>
2020-12-16 21:29:02 +0700

    rev4 tambah content en

--- plugins/js/toggle.js
+++ plugins/js/toggle.js
@@ -1,10 +1,8 @@
-
-
-
 function hyClick() {
-    const x = document.createElement("a");
-    // x.setAttribute("rel", "stylesheet");
-    // x.setAttribute("type", "text/css");
-    x.setAttribute("href", "{{ $translation.Permalink | relURL }}");
-    document.head.appendChild(x);
+    let x = document.getElementById("get");
+    if (x.href === "/id/") {
+        x.href = "/id/";
+    } else {
+        x.innerHTML = "Hello";
+    }
 }
