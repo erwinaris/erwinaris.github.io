551feb7fa67ae7a17bdc0df9d976fb1561d32634
erwinaris <erwinaris442@gmail.com>
2021-07-28 09:33:11 +0700

    remove all files public

--- plugins/js/initialize.js
+++
@@ -1,8 +0,0 @@
-const dropDown = document.querySelectorAll('.dropdown-trigger');
-M.Dropdown.init(dropDown);
-
-const collapse = document.querySelectorAll('.collapsible');
-M.Collapsible.init(collapse);
-
-const box = document.querySelectorAll('.materialboxed');
-M.Materialbox.init(box);
--- plugins/js/toggle.js
+++
@@ -1,8 +0,0 @@
-function hyClick() {
-    let x = document.getElementById("get");
-    if (x.href === "/id/") {
-        x.href = "/id/";
-    } else {
-        x.innerHTML = "Hello";
-    }
-}
